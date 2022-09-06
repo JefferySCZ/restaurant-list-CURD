dc4cc42ae98fcb8d35e066c6bf224811949b6fbf
JefferySCZ <jeffery.sim.cz@gmail.com>
2022-09-06 11:14:05 +0800

    feat: ad dhandlebars

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 //require packages used in the project
 const express = require('express')
+const mongoose = require('mongoose')
+console.log(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 const app = express()
 const port = 3000
@@ -11,42 +28,42 @@ const expbhs = require('express-handlebars')
 const restaurantList = require('./restaurant.json').results
 
 //setting template engine
-app.engine('handlebars', expbhs({ defaultLayout: 'main'}))
+app.engine('handlebars', expbhs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
-//setting static files
-app.use(express.static('public'))
-
 
 //routes  setting
 /// main page
 app.get('/', (req, res) => {
-  res.render('index', {restaurants : restaurantList})
+  res.render('index', { restaurants: restaurantList })
 })
 
 ///render show by id
-app.get('/restaurants/:restaurant_id', (req,res) =>{
+app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant_id = req.params.restaurant_id
-  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === restaurant_id)
-res.render('show', {restaurant : restaurant})
+  const restaurant = restaurantList.find(
+    (restaurant) => restaurant.id.toString() === restaurant_id
+  )
+  res.render('show', { restaurant: restaurant })
 })
 
 //search routes
 app.get('/search/', (req, res) => {
   const keyword = req.query.keyword.trim()
 
+  const filterRestaurants = restaurantList.filter(
+    (item) =>
+      item.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      item.category.includes(keyword)
+  )
 
-  const filterRestaurants = restaurantList.filter(item => item.name.toLowerCase().includes(keyword.toLowerCase()) || item.category.includes(keyword))
-
-//result cant find
+  //result cant find
   if (filterRestaurants.length === 0) {
-    res.render('notFound', {keyword})
+    res.render('notFound', { keyword })
   } else {
-    res.render('index', { restaurants: filterRestaurants, keyword})
+    res.render('index', { restaurants: filterRestaurants, keyword })
   }
 })
-
 
 app.listen(port, () => {
   console.log(`Listening  on http://localhost:${port}`)
